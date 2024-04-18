9f650636719ef24d630413fb5c3c801ae933d581
Cam Harris <cam.harris@coinbase.com>
2024-04-18 12:03:51 -0700

    flushed out factory with more functions and test coverage

--- src/BaseSplitFactory.sol
+++ src/BaseSplitFactory.sol
@@ -5,21 +5,97 @@ import "./BaseSplitter.sol"; // Import the BaseSplitter contract
 
 contract BaseSplitFactory {
     address public owner;
-    address public splitter;
-    address[] public splitters;
+    uint256 public fee;
+
+    struct Splitter {
+        address owner;
+        address[] parties;
+        uint256[] shares;
+        address splitterAddress;
+    }
+
+    Splitter[] public splitters;
 
     constructor(address _owner) {
         owner = _owner;
     }
 
+    // Set protocol fee in basis points (1 basis point = 0.01%)
+    function setFee(uint256 _feeInBasisPoints) public {
+        require(msg.sender == owner, "Only owner can set fee");
+        require(_feeInBasisPoints <= 10000, "Fee cannot be more than 100%");
+        fee = _feeInBasisPoints;
+        // uint256 feeAmount = amount * fee / 10000;
+    }
+
+    // get protocol fee
+    function getFee() public view returns (uint256) {
+        return fee;
+    }
+
     function registerSplitter(
         address splitterOwner,
-        address[] memory parties
+        address[] memory parties,
+        uint256[] memory shares
     ) public {
         address factory = address(this);
-        // require(msg.sender == owner, "Only owner can create splitter");
-        require(splitter == address(0), "Splitter already created");
-        splitter = address(new BaseSplitter(splitterOwner, parties, factory)); // Create a new instance of BaseSplitter
-        splitters.push(splitter);
+        
+        require(parties.length == shares.length, "Parties and shares length mismatch");
+        // Verify shares are not greater than 100
+        uint256 totalShares = 0;
+        for (uint256 i = 0; i < shares.length; i++) {
+            require(shares[i] <= 100, "Shares must be less than or equal to 100");
+            totalShares += shares[i];
+        }
+        require(totalShares == 100, "Total shares must be equal to 100");
+
+        address splitterAddress = address(new BaseSplitter(splitterOwner, parties, factory)); // Create a new instance of BaseSplitter
+        splitters.push(Splitter(splitterOwner, parties, shares, splitterAddress)); // Add the new splitter to the list of splitters
     }
+
+    // Get splitters by owner
+    function getSplittersByOwner(address splitterOwner) public view returns (Splitter[] memory) {
+        Splitter[] memory ownerSplitters = new Splitter[](splitters.length);
+        uint256 count = 0;
+        for (uint256 i = 0; i < splitters.length; i++) {
+            if (splitters[i].owner == splitterOwner) {
+                ownerSplitters[count] = splitters[i];
+                count++;
+            }
+        }
+        
+        if (count == 0) {
+            return new Splitter[](0); // Return an empty array if no splitters found
+        } else {
+            Splitter[] memory result = new Splitter[](count);
+            for (uint256 i = 0; i < count; i++) {
+                result[i] = ownerSplitters[i];
+            }
+            return result;
+        }
+    }
+
+    // Get all splitters
+    function getAllSplitters() public view returns (Splitter[] memory) {
+        return splitters;
+    }
+
+    // Get count of all splitters
+    function getSplitterCount() public view returns (uint256) {
+        return splitters.length;
+    }
+
+    // Get splitter by address
+    function getSplitterByAddress(address splitterAddress) public view returns (Splitter memory) {
+        for (uint256 i = 0; i < splitters.length; i++) {
+            if (splitters[i].splitterAddress == splitterAddress) {
+                return splitters[i];
+            }
+        }
+        revert("Splitter not found");
+    }
+
+    // make payable to receive fees
+    receive() external payable {}
+
 }
--- test/BaseSplitFactory.t.sol
+++ test/BaseSplitFactory.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
+import "forge-std/Console.sol";
 import {Test, console2} from "forge-std/Test.sol";
 import {BaseSplitFactory} from "../src/BaseSplitFactory.sol";
 
@@ -11,13 +12,94 @@ contract BaseSplitFactoryTest is Test {
         factory = new BaseSplitFactory(address(this));
     }
 
+    function test_SetFee() public {
+        factory.setFee(3);
+        assertEq(factory.getFee(), 3);
+    }
+
     function test_RegisterSplitter() public {
-        address[] memory parties = new address[](3);
+        address[] memory parties = new address[](4);
         parties[0] = address(0x1);
         parties[1] = address(0x2);
         parties[2] = address(0x3);
+        parties[3] = address(0x4);
 
-        factory.registerSplitter(address(this), parties);
-        assertEq(factory.splitters(0), factory.splitter());
+        uint256[] memory shares = new uint256[](4);
+        shares[0] = 25;
+        shares[1] = 25;
+        shares[2] = 25;
+        shares[3] = 25;
+
+        factory.registerSplitter(address(this), parties, shares);
+        assertEq(factory.getSplitterCount(), 1);
+    }
+
+    function test_GetSplittersByOwner() public {
+        address[] memory parties = new address[](2);
+        parties[0] = address(0x1);
+        parties[1] = address(0x2);
+
+
+        uint256[] memory shares = new uint256[](2);
+        shares[0] = 50;
+        shares[1] = 50;
+
+        factory.registerSplitter(address(this), parties, shares);
+        assertEq(factory.getSplitterCount(), 1);
+
+        BaseSplitFactory.Splitter[] memory splitters = factory.getSplittersByOwner(address(this));
+        console2.log("Splitters length: ", splitters.length);
+        assertEq(splitters.length, 1);
+    }
+
+    function test_GetSplittersByOwner_InvalidOwner() public {
+        address[] memory parties = new address[](2);
+        parties[0] = address(0x1);
+        parties[1] = address(0x2);
+
+        uint256[] memory shares = new uint256[](2);
+        shares[0] = 50;
+        shares[1] = 50;
+
+        factory.registerSplitter(address(this), parties, shares);
+        assertEq(factory.getSplitterCount(), 1);
+
+        BaseSplitFactory.Splitter[] memory splitters = factory.getSplittersByOwner(address(0x0));
+        console2.log("Splitters length: ", splitters.length);
+        assertEq(splitters.length, 0);
+    }
+
+    function test_GetAllSplitters() public {
+        address[] memory parties = new address[](2);
+        parties[0] = address(0x1);
+        parties[1] = address(0x2);
+
+        uint256[] memory shares = new uint256[](2);
+        shares[0] = 50;
+        shares[1] = 50;
+
+        factory.registerSplitter(address(this), parties, shares);
+        assertEq(factory.getSplitterCount(), 1);
+
+        BaseSplitFactory.Splitter[] memory splitters = factory.getAllSplitters();
+        console2.log("Splitters length: ", splitters.length);
+        assertEq(splitters.length, 1);
+    }
+
+    function test_GetSplitterByAddress() public {
+        address[] memory parties = new address[](2);
+        parties[0] = address(0x1);
+        parties[1] = address(0x2);
+
+        uint256[] memory shares = new uint256[](2);
+        shares[0] = 50;
+        shares[1] = 50;
+
+        factory.registerSplitter(address(this), parties, shares);
+        assertEq(factory.getSplitterCount(), 1);
+
+        BaseSplitFactory.Splitter memory splitter = factory.getSplitterByAddress(factory.getAllSplitters()[0].splitterAddress);
+        console2.log("Splitter address: ", splitter.splitterAddress);
+        assertEq(splitter.owner, address(this));
     }
 }
